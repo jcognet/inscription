80c88960ae05b77cc78e210206cc60586afcdb89
Jerome <jcognet@gmail.com>
2017-10-09 09:11:46 +0200

    close #10

---
+++ web/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    // Mise en place d'événements
+    changeSort();
+    alerteSiSuppression();
+    changeSaison();
+});
+// Gestion des erreurs ajax
+$( document ).ajaxError(function( event, request, settings ) {
+    console.log('*******Erreur*******');
+    console.log(event);
+    console.log(request);
+    console.log(settings);
+    console.log('********************');
+});
+// Lance l'ajax
+function setAjaxWorking(divId) {
+    var divAjax = $("#" + divId);
+    var loader = $("#loader_ajax").clone();
+    divAjax.addClass('ajax');
+    loader.css('margin-left', divAjax.width()/2 + 'px');
+    loader.css('margin-top', divAjax.height()/2 + 'px');
+    loader.addClass('loader');
+    loader.attr('id', 'loader_ajax_' + divId).show().prependTo("#" + divId);
+}
+// Arrête l'Ajax
+function unsetAjaxWorking(divId) {
+    $("#" + divId).removeClass('ajax');
+    $("#loader_ajax_" + divId).remove();
+}
+// Gère les tris
+function changeSort(){
+    $('.sorted .asc').each(function(){
+        $(this).append('<span class="glyphicon glyphicon-arrow-down"></span>');
+    });
+    $('.sorted .desc').each(function(){
+        $(this).append('<span class="glyphicon glyphicon-arrow-up"></span>');
+    });
+}
+// Gère l'alerte de suppression
+function alerteSiSuppression(){
+    $('.lnk_suppression').each(function(){
+        $(this).click( function(e){
+            if(!confirm('Voulez-vous vraiment supprimer cet élément ? ')){
+                e.preventDefault();
+                return false;
+            }
+        });
+    });
+}
+function changeSaison(){
+    $("#ddlSelectSaison").on("change", function(){
+        var form =$("#ddlSelectSaison").closest('form');
+        form.submit();
+    });
+}
